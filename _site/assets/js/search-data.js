6b67a79767d7c34ed7834a2c860d7172bf05a19b
Luke Marney <marneylc@gmail.com>
2019-11-21 10:46:25 -0800

    finish up simple theme format

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Nadi Shodhana",
+        "excerpt":"Sit comfortably with the spine straight. With vishnu mudra block the right nostril. Inhale through the left nostril to the count of 4. Close both nostrils and retain the breath for the count of 16. Open the right nostril and exhale to the count of 8. Alternate nostrils for four...","categories": ["pranayama"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/pranayama/2019/11/21/welcome-to-jekyll.html"
+      }]
